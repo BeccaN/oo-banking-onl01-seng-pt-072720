5b5464ab359111869ff17c8955e7661d7499e5ed
Rebecca Congi <rncongi@gmail.com>
2020-08-25 01:55:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,30 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+  
+  def initialize(sender, receiver, amount)
+    @receiver = receiver
+    @sender = sender
+    @amount = amount
+    @status = "pending"
+  end   
+  
+  def valid?
+    if self.sender.valid? && self.receiver.valid? 
+      return true 
+    end 
+  end   
+  
+  def execute_transaction
+    if @status == "pending" && @sender.balance > @amount
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    else
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end 
+  end   
+  
 end
